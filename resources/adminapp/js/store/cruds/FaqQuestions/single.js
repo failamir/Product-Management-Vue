b591e1f9c7cecd94d72f0a9de5f4079da6a39128
Restyled.io <commits@restyled.io>
2023-03-15 23:41:54 +0000

    Restyled by clang-format

--- resources/adminapp/js/store/cruds/FaqQuestions/single.js
+++ resources/adminapp/js/store/cruds/FaqQuestions/single.js
@@ -9,25 +9,22 @@ function initialState() {
       updated_at: '',
       deleted_at: ''
     },
-    lists: {
-      category: []
-    },
-    loading: false
+        lists: {category: []}, loading: false
   }
 }
 
 const route = 'faq-questions'
 
 const getters = {
-  entry: state => state.entry,
-  lists: state => state.lists,
-  loading: state => state.loading
+  entry : state => state.entry,
+  lists : state => state.lists,
+  loading : state => state.loading
 }
 
 const actions = {
-  storeData({ commit, state, dispatch }) {
+  storeData({commit, state, dispatch}) {
     commit('setLoading', true)
-    dispatch('Alert/resetState', null, { root: true })
+    dispatch('Alert/resetState', null, {root : true})
 
     return new Promise((resolve, reject) => {
       let params = objectToFormData(state.entry, {
@@ -41,13 +38,11 @@ const actions = {
         })
         .catch(error => {
           let message = error.response.data.message || error.message
-          let errors = error.response.data.errors
+      let errors = error.response.data.errors
 
-          dispatch(
-            'Alert/setAlert',
-            { message: message, errors: errors, color: 'danger' },
-            { root: true }
-          )
+      dispatch('Alert/setAlert',
+               {message : message, errors : errors, color : 'danger'},
+               {root : true})
 
           reject(error)
         })
@@ -56,16 +51,16 @@ const actions = {
         })
     })
   },
-  updateData({ commit, state, dispatch }) {
+  updateData({commit, state, dispatch}) {
     commit('setLoading', true)
-    dispatch('Alert/resetState', null, { root: true })
+    dispatch('Alert/resetState', null, {root : true})
 
     return new Promise((resolve, reject) => {
       let params = objectToFormData(state.entry, {
         indices: true,
         booleansAsIntegers: true
       })
-      params.set('_method', 'PUT')
+    params.set('_method', 'PUT')
       axios
         .post(`${route}/${state.entry.id}`, params)
         .then(response => {
@@ -73,13 +68,11 @@ const actions = {
         })
         .catch(error => {
           let message = error.response.data.message || error.message
-          let errors = error.response.data.errors
+      let errors = error.response.data.errors
 
-          dispatch(
-            'Alert/setAlert',
-            { message: message, errors: errors, color: 'danger' },
-            { root: true }
-          )
+      dispatch('Alert/setAlert',
+               {message : message, errors : errors, color : 'danger'},
+               {root : true})
 
           reject(error)
         })
@@ -88,82 +81,42 @@ const actions = {
         })
     })
   },
-  setCategory({ commit }, value) {
-    commit('setCategory', value)
+  setCategory({commit}, value) { commit('setCategory', value) },
+  setQuestion({commit}, value) { commit('setQuestion', value) },
+  setAnswer({commit}, value) { commit('setAnswer', value) },
+  setCreatedAt({commit}, value) { commit('setCreatedAt', value) },
+  setUpdatedAt({commit}, value) { commit('setUpdatedAt', value) },
+  setDeletedAt({commit}, value) { commit('setDeletedAt', value) },
+  fetchCreateData({commit}) {
+    axios.get(`${route}/create`)
+        .then(response => {commit('setLists', response.data.meta)})
   },
-  setQuestion({ commit }, value) {
-    commit('setQuestion', value)
-  },
-  setAnswer({ commit }, value) {
-    commit('setAnswer', value)
-  },
-  setCreatedAt({ commit }, value) {
-    commit('setCreatedAt', value)
-  },
-  setUpdatedAt({ commit }, value) {
-    commit('setUpdatedAt', value)
-  },
-  setDeletedAt({ commit }, value) {
-    commit('setDeletedAt', value)
-  },
-  fetchCreateData({ commit }) {
-    axios.get(`${route}/create`).then(response => {
-      commit('setLists', response.data.meta)
-    })
-  },
-  fetchEditData({ commit, dispatch }, id) {
+  fetchEditData({commit, dispatch}, id) {
     axios.get(`${route}/${id}/edit`).then(response => {
       commit('setEntry', response.data.data)
       commit('setLists', response.data.meta)
     })
   },
-  fetchShowData({ commit, dispatch }, id) {
-    axios.get(`${route}/${id}`).then(response => {
-      commit('setEntry', response.data.data)
-    })
+  fetchShowData({commit, dispatch}, id) {
+    axios.get(`${route}/${id}`)
+        .then(response => {commit('setEntry', response.data.data)})
   },
-  resetState({ commit }) {
-    commit('resetState')
-  }
+  resetState({commit}) { commit('resetState') }
 }
 
 const mutations = {
-  setEntry(state, entry) {
-    state.entry = entry
-  },
-  setCategory(state, value) {
-    state.entry.category_id = value
-  },
-  setQuestion(state, value) {
-    state.entry.question = value
-  },
-  setAnswer(state, value) {
-    state.entry.answer = value
-  },
-  setCreatedAt(state, value) {
-    state.entry.created_at = value
-  },
-  setUpdatedAt(state, value) {
-    state.entry.updated_at = value
-  },
-  setDeletedAt(state, value) {
-    state.entry.deleted_at = value
-  },
-  setLists(state, lists) {
-    state.lists = lists
-  },
-  setLoading(state, loading) {
-    state.loading = loading
-  },
-  resetState(state) {
-    state = Object.assign(state, initialState())
-  }
+  setEntry(state, entry) { state.entry = entry },
+  setCategory(state, value) { state.entry.category_id = value },
+  setQuestion(state, value) { state.entry.question = value },
+  setAnswer(state, value) { state.entry.answer = value },
+  setCreatedAt(state, value) { state.entry.created_at = value },
+  setUpdatedAt(state, value) { state.entry.updated_at = value },
+  setDeletedAt(state, value) { state.entry.deleted_at = value },
+  setLists(state, lists) { state.lists = lists },
+  setLoading(state, loading) { state.loading = loading },
+  resetState(state) { state = Object.assign(state, initialState()) }
 }
 
 export default {
-  namespaced: true,
-  state: initialState,
-  getters,
-  actions,
-  mutations
+  namespaced: true, state: initialState, getters, actions, mutations
 }
--- resources/adminapp/js/store/store.js
+++ resources/adminapp/js/store/store.js
@@ -1,26 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import Alert from './modules/alert'
-import I18NStore from './modules/i18n'
-
+import FaqCategoriesIndex from './cruds/FaqCategories'
+import FaqCategoriesSingle from './cruds/FaqCategories/single'
+import FaqQuestionsIndex from './cruds/FaqQuestions'
+import FaqQuestionsSingle from './cruds/FaqQuestions/single'
 import PermissionsIndex from './cruds/Permissions'
 import PermissionsSingle from './cruds/Permissions/single'
 import RolesIndex from './cruds/Roles'
 import RolesSingle from './cruds/Roles/single'
 import UsersIndex from './cruds/Users'
 import UsersSingle from './cruds/Users/single'
-import FaqCategoriesIndex from './cruds/FaqCategories'
-import FaqCategoriesSingle from './cruds/FaqCategories/single'
-import FaqQuestionsIndex from './cruds/FaqQuestions'
-import FaqQuestionsSingle from './cruds/FaqQuestions/single'
+import Alert from './modules/alert'
+import I18NStore from './modules/i18n'
 
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
-  modules: {
+  modules : {
     Alert,
     I18NStore,
     PermissionsIndex,
@@ -34,5 +33,5 @@ export default new Vuex.Store({
     FaqQuestionsIndex,
     FaqQuestionsSingle
   },
-  strict: debug
+  strict : debug
 })
